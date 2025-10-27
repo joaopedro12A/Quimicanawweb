5ba4a572ca9d627b81d28fe3cdc66011d0f4f878
joaopedro12A <161172343+joaopedro12A@users.noreply.github.com>
2025-10-27 19:53:04 -0300

    Update pergunta3.js

--- pergunta3.js
+++ pergunta3.js
@@ -115,7 +115,7 @@ function finalizarQuiz() {
     medalhaImg.src = "Medalhas/medalha de prata.png";
     mensagem.textContent = `Muito bem! Você acertou ${contadorAcertos}/${perguntas.length} questões.`;
   } else if (contadorAcertos === perguntas.length - 2) {
-    titulo.textContent = "Medalha de Cu + Zn!";
+    titulo.textContent = "Medalha de Cu + Sn!";
     medalhaImg.src = "Medalhas/medalha de bronze.png";
     mensagem.textContent = `Bom esforço! Você acertou ${contadorAcertos}/${perguntas.length} questões.`;
   } else {
